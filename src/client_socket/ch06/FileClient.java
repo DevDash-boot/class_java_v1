aa0a6d75cc0a4e841333871a7deb13ce8545a248
DebDash-boot <jeonghs1570@gmail.com>
2026-08-18 14:05:13 +0900

    ..

--- src/client_socket/ch06/FileClient.java
+++ src/client_socket/ch06/FileClient.java
@@ -20,7 +20,7 @@ public class FileClient {
         Scanner sc = new Scanner(System.in);
         System.out.println("전송할 파일 경로 예) C:\\work_java\\test.txt");
         // String filePath = sc.nextLine();
-        String filePath = "C:\\jhg\\work_space\\java_class_1\\java_v1\\assets\\a.txt";
+        String filePath = "C:\\jhg\\work_space\\java_class_1\\assets\\a.txt";
 
         File file = new File(filePath);
         if (!file.exists() || !file.isFile()) {
